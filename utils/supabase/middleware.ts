ef2460152b2c54208b1c67f3154d7c8f3c789be8
Aaron J. Girton <agirton@intradark.com>
2024-09-16 18:18:28 +1000

    feat(middleware): Update Supabase middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -48,6 +48,10 @@ export const updateSession = async (request: NextRequest) => {
       return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
+    if (request.nextUrl.pathname === '/' && user.error) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
     return response;
   } catch (e) {
     return NextResponse.next({
