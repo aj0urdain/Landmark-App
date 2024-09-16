265ead9bf8be0227fdcac87b18bfe5677beb2bdd
Aaron J. Girton <agirton@intradark.com>
2024-09-16 18:18:15 +1000

    chore(config): Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Add this line to ignore hydration warnings
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'www.burgessrawson.com.au',
+        protocol: 'https',
+      },
+      {
+        hostname: 'dodfdwvvwmnnlntpnrec.supabase.co',
+        protocol: 'https',
+      },
+      {
+        hostname: 'localhost',
+        protocol: 'http',
+      },
+      {
+        hostname: 'static.ffx.io',
+        protocol: 'https',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
