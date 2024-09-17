b596a484144213777ee448e208a27891c3686d42
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-17 23:10:00 +1000

    feat(colors): define portfolio color scheme

---
+++ styles/PortfolioPage/colors.ts
@@ -0,0 +1,28 @@
+export const portfolioColors = {
+  // headline text
+  headline: '#1c374a',
+
+  // address text
+  address: '#1c374a',
+
+  // financeCopy text
+  financeCopy: '#1c374a',
+
+  // propertyCopy text
+  propertyCopy: '#626365',
+
+  // contact text
+  contactHeader: '#626365',
+  agents: '#626365',
+
+  // saleType text
+  saleType: '#626365',
+  saleInfo: '#626365',
+
+  // border
+  border: '#a7a9ac',
+} as const;
+
+module.exports = {
+  portfolioColors,
+};
