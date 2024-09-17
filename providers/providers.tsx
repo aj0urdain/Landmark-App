97d9acb74f9d6f79d22ead5e1d84203b62ac8f81
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-17 23:11:52 +1000

    feat: add provider components

---
+++ providers/providers.tsx
@@ -0,0 +1,43 @@
+'use client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { getQueryClient } from '@/utils/get-query-client';
+import { createClient } from '@/utils/supabase/client';
+import { useEffect } from 'react';
+import type * as React from 'react';
+
+function RealtimeUpdates() {
+  const queryClient = getQueryClient();
+  const supabase = createClient();
+
+  useEffect(() => {
+    const subscription = supabase
+      .channel('public:user_profiles')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'user_profiles' },
+        () => {
+          queryClient.invalidateQueries({ queryKey: ['userProfile'] });
+        }
+      )
+      .subscribe();
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [queryClient, supabase]);
+
+  return null;
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RealtimeUpdates />
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
