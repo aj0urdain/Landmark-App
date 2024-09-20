9b98d0a6930f768bab9fe79571a259a525336a90
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-21 09:59:31 +1000

    fix(supabase): update admin utility functions

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAdminKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvZGZkd3Z2d21ubmxudHBucmVjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjM1OTg5NSwiZXhwIjoyMDQxOTM1ODk1fQ.dt5iMPl211WwBgMHRAWu5xSeCOjUrkJ7WdWbdjVcRmg" ||
-  "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! || "";
+const supabaseAdminKey = process.env.SUPABASE_SERVICE_ROLE_KEY! || "";
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseAdminKey);
