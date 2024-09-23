7d5e7c851194e9ef8e2992bbb366bef37bda585c
Aaron J. Girton <agirton@intradark.com>
2024-09-23 11:50:18 +1000

    feat(properties): update properties page

--- app/(main)/properties/page.tsx
+++ app/(main)/properties/page.tsx
@@ -1,5 +1,4 @@
 import PropertiesContent from "@/components/templates/ListingsPage/PropertiesContent/PropertiesContent";
-import React from "react";
 
 const PropertiesPage = () => {
   return <PropertiesContent />;
