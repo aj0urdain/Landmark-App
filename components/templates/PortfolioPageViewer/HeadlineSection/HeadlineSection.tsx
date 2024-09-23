a167c7bf5b14343ab32fb9e2787d23986b4a48f0
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-24 00:45:59 +1000

    refactor: update headline section

--- components/templates/PortfolioPageViewer/HeadlineSection/HeadlineSection.tsx
+++ components/templates/PortfolioPageViewer/HeadlineSection/HeadlineSection.tsx
@@ -1,16 +1,30 @@
 import { useQueryClient, useQuery } from "@tanstack/react-query";
 import { textAlgorithm } from "@/utils/sandbox/document-generator/portfolio-page/textAlgorithm";
-import { headlineDataOptions } from "@/utils/sandbox/document-generator/portfolio-page/portfolio-queries";
+import {
+  headlineDataOptions,
+  logoDataOptions,
+} from "@/utils/sandbox/document-generator/portfolio-page/portfolio-queries";
 
 const HeadlineSection = () => {
   const queryClient = useQueryClient();
+
   const { data: headlineData } = useQuery(headlineDataOptions);
+  const { data: logoData } = useQuery(logoDataOptions);
 
   const textProps = textAlgorithm("headline", queryClient);
 
+  const getHeadlineWidth = () => {
+    if (!logoData || logoData.logoCount === 0) return "90.5%";
+    if (logoData.logoCount === 1) return "76.5%";
+    return "62.5%";
+  };
+
+  const headlineWidth = getHeadlineWidth();
+
   return (
     <div
-      className={`absolute left-[4.75%] top-[68%] flex h-[5.5%] w-[62.5%] font-medium`}
+      className={`absolute left-[4.75%] top-[68%] flex h-[5.5%] font-medium`}
+      style={{ width: headlineWidth }}
     >
       <span className={textProps.getTailwind()} style={textProps.getStyle()}>
         {headlineData?.headline ||
