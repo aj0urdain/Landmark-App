5e27f07c570541759228274617690c929ef572c9
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-29 22:04:15 +1000

    refactor: remove unused pages

--- app/(main)/directory/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DirectoryPage = () => {
-  return <div>DirectoryPage</div>;
-};
-
-export default DirectoryPage;
--- app/(main)/listings/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ListingsContent from "@/components/templates/ListingsPage/ListingsContent/ListingsContent";
-
-export default function ListingsPage() {
-  return <ListingsContent />;
-}
