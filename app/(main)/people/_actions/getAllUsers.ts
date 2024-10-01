8ccc59318fc3667fa62bfbd830fc81b4147da8e5
Aaron J. Girton <agirton@intradark.com>
2024-10-01 18:55:22 +1000

    refactor: remove unused pages and actions

--- app/(main)/people/_actions/getAllUsers.ts
+++
@@ -1,9 +0,0 @@
-import { queryOptions } from "@tanstack/react-query";
-import { selectAllUsers } from "@/utils/use-cases/all-users/all-users";
-
-export const getAllUsers = queryOptions({
-  queryKey: ["allUsers"],
-  queryFn: async () => {
-    return await selectAllUsers();
-  },
-});
