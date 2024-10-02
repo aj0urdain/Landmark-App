0036fd7b313eb99ec94fea57cb8c03eaec26f54c
Aaron J. Girton <agirton@intradark.com>
2024-10-02 19:26:47 +1000

    refactor(supabase): Update Supabase client configuration

--- utils/supabase/client.tsx
+++ utils/supabase/client.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { Database } from "@/types/supabaseTypes";
 import { createBrowserClient as createSupabaseBrowserClient } from "@supabase/ssr";
 
 export function createBrowserClient() {
-  return createSupabaseBrowserClient(
+  return createSupabaseBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
