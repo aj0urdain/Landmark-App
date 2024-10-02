26acb6ec802d8fbbacfd79bc51f359bab647fe53
Aaron J. Girton <agirton@intradark.com>
2024-10-02 19:26:38 +1000

    feat(create): Enhance create page functionality

--- app/(main)/create/page.tsx
+++ app/(main)/create/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create a new task</CardTitle>
+        <CardDescription>Choose a task to create.</CardDescription>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default CreatePage;
