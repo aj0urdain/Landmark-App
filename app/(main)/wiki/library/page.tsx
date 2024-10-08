536e6e36503b0459a3c32e2661ada10ca9abf4bb
Aaron J. Girton <agirton@intradark.com>
2024-10-08 12:34:16 +1100

    refactor(wiki): update library page

--- app/(main)/wiki/library/page.tsx
+++ app/(main)/wiki/library/page.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const LibraryPage = () => {
@@ -6,33 +12,67 @@ const LibraryPage = () => {
     <Card>
       <CardHeader>
         <CardTitle>Library</CardTitle>
+        <CardDescription>
+          The layout of this page is still under development!
+        </CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>
-          1. Portfolio Magazine library 2. Industry Insights reports library 3.
-          Industry Snapshots library 4. Auction results library 5. Capability
-          statement library 6. IM/submission
-        </p>
-
+      <CardContent className="flex flex-col gap-8">
         {/* Premium IM PDF, Premium IM Submission, Premium IM Working Folder (IM + SUBMISSION), Market Share */}
+        <h2>Premium Content</h2>
+
+        {/* Portfolio Magazine Library */}
+        <h2>Portfolio Magazine Library</h2>
+
         {/* Industry Reports */}
+        <h2>Industry Reports</h2>
+
         {/* Industry Snapshots */}
+        <h2>Industry Snapshots</h2>
+
         {/* Auction Results */}
+        <h2>Auction Results</h2>
+
         {/* Capability Statement */}
+        <h2>Capability Statement</h2>
+
         {/* IM/Submission */}
-        {/* Icon Library */}
-        {/* Needs to be maintainable pngs */}
+        <h2>IM/Submission</h2>
+
+        {/* Icon Library (needs to be maintainable pngs) */}
+        <h2>Icon Library</h2>
+
         {/* Fonts */}
+        <h2>Fonts</h2>
+
         {/* Colors */}
+        <h2>Colors</h2>
+
         {/* Typography */}
+        <h2>Typography</h2>
+
         {/* Icons */}
+        <h2>Icons</h2>
+
         {/* Images */}
+        <h2>Images</h2>
+
         {/* Videos */}
+        <h2>Videos</h2>
+
         {/* Audio */}
+        <h2>Audio</h2>
+
         {/* Documents */}
+        <h2>Documents</h2>
+
         {/* Spreadsheets */}
+        <h2>Spreadsheets</h2>
+
         {/* Presentations */}
+        <h2>Presentations</h2>
+
         {/* Databases */}
+        <h2>Databases</h2>
       </CardContent>
     </Card>
   );
