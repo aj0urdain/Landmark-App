0d43795450be1dec3695544a0d4fe564e280fa05
Aaron J. Girton <agirton@intradark.com>
2024-10-08 12:34:17 +1100

    refactor(wiki): update people page

--- app/(main)/wiki/people/page.tsx
+++ app/(main)/wiki/people/page.tsx
@@ -1,11 +1,26 @@
 "use server";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { UsersPage } from "./users-page";
 
 export default async function PeoplePage() {
   return (
-    <main>
-      <UsersPage />
-    </main>
+    <Card>
+      <CardHeader>
+        <CardTitle>People</CardTitle>
+        <CardDescription>
+          The layout of this page is still under development!
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <UsersPage />
+      </CardContent>
+    </Card>
   );
 }
