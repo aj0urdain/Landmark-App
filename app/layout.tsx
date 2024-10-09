c57d216e8a98128cc8536d643d7e365657a3e6e3
Aaron J. Girton <agirton@intradark.com>
2024-10-09 19:15:00 +1100

    chore: Update app layout configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <Providers>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
