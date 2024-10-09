049c70220d63b4c787d00027b3d1c1b1f58de5f3
Aaron J. Girton <agirton@intradark.com>
2024-10-09 19:14:55 +1100

    refactor: Update Events page to use new components

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,8 +1,19 @@
-import { Card } from "@/components/ui/card";
+"use client";
+
 import React from "react";
+import { Card } from "@/components/ui/card";
+
+import { CompanyCalendar } from "@/components/molecules/CompanyCalendar/CompanyCalendar";
 
 const EventsPage = () => {
-  return <Card className="h-full w-full p-4">EventsPage</Card>;
+  return (
+    <Card className="w-full p-4">
+      <h1 className="mb-4 text-2xl font-bold">Company Events</h1>
+      <div className="grid h-full grid-cols-1 gap-4">
+        <CompanyCalendar />
+      </div>
+    </Card>
+  );
 };
 
 export default EventsPage;
