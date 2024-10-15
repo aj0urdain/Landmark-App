d13a69ce3ae6c418142781b66d6e0784faeee4b4
Aaron J. Girton <agirton@intradark.com>
2024-10-15 15:24:09 +1100

    style(filter): update EventTypeFilterBadge styling

--- components/atoms/EventTypeFilterBadge/EventTypeFilterBadge.tsx
+++ components/atoms/EventTypeFilterBadge/EventTypeFilterBadge.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { getEventTypeInfo } from "@/utils/getEventTypeInfo";
+import { getEventTypeInfo } from "@/utils/eventTypeInfo";
 
 interface EventTypeFilterBadgeProps {
   className?: string;
