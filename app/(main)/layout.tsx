4efdb7540bf418a79320f4a05ecd890893421ea0
Aaron J. Girton <agirton@intradark.com>
2024-10-25 12:32:21 +1100

    refactor(layout): modify main layout structure

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,19 @@
-"use client";
+'use client';
 
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Sidebar } from "@/components/molecules/Sidebar/Sidebar";
-import { Button } from "@/components/ui/button";
-import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
-import { Header } from "@/components/molecules/Header/Header";
-import { UserProfileManager } from "@/components/atoms/UserProfileManager/UserProfileManager";
-import { Inbox } from "@/components/molecules/Inbox/Inbox";
-import { AccessControl } from "@/components/atoms/AccessControl/AccessControl";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { Sidebar } from '@/components/molecules/Sidebar/Sidebar';
+import { Button } from '@/components/ui/button';
+import { PanelLeftClose, PanelLeftOpen } from 'lucide-react';
+import { Header } from '@/components/molecules/Header/Header';
+import { UserProfileManager } from '@/components/atoms/UserProfileManager/UserProfileManager';
+import { Inbox } from '@/components/molecules/Inbox/Inbox';
+import { AccessControl } from '@/components/atoms/AccessControl/AccessControl';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 TimeAgo.addDefaultLocale(en);
 
@@ -20,50 +21,51 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const [isLeftSidebarCollapsed, setIsLeftSidebarCollapsed] = useState(true);
   const [isInboxCollapsed, setIsInboxCollapsed] = useState(true);
 
-  const toggleLeftSidebar = () =>
-    setIsLeftSidebarCollapsed(!isLeftSidebarCollapsed);
+  const toggleLeftSidebar = () => setIsLeftSidebarCollapsed(!isLeftSidebarCollapsed);
   const toggleInbox = () => setIsInboxCollapsed(!isInboxCollapsed);
 
   return (
-    <div className="flex h-screen overflow-hidden">
-      <UserProfileManager />
-      <div
-        className={`relative hidden flex-col transition-all duration-300 ease-in-out xl:flex ${
-          isLeftSidebarCollapsed ? "w-20" : "w-64"
-        }`}
-      >
-        <Sidebar isCollapsed={isLeftSidebarCollapsed} />
-        <Button
-          variant="ghost"
-          size="icon"
-          className="absolute -right-4 top-1/2 -translate-y-1/2 transform rounded-full shadow-md"
-          onClick={toggleLeftSidebar}
+    <TooltipProvider>
+      <div className="flex h-screen overflow-hidden">
+        <UserProfileManager />
+        <div
+          className={`relative hidden flex-col transition-all duration-300 ease-in-out xl:flex ${
+            isLeftSidebarCollapsed ? 'w-20' : 'w-64'
+          }`}
         >
-          {isLeftSidebarCollapsed ? (
-            <PanelLeftOpen className="h-4 w-4" />
-          ) : (
-            <PanelLeftClose className="h-4 w-4" />
-          )}
-          <span className="sr-only">Toggle sidebar</span>
-        </Button>
+          <Sidebar isCollapsed={isLeftSidebarCollapsed} />
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute -right-4 top-1/2 -translate-y-1/2 transform rounded-full shadow-md"
+            onClick={toggleLeftSidebar}
+          >
+            {isLeftSidebarCollapsed ? (
+              <PanelLeftOpen className="h-4 w-4" />
+            ) : (
+              <PanelLeftClose className="h-4 w-4" />
+            )}
+            <span className="sr-only">Toggle sidebar</span>
+          </Button>
+        </div>
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <Header />
+          <ScrollArea className="flex-1">
+            <main className="mx-4 flex h-full w-full items-center justify-center py-4 2xl:mx-auto">
+              <div className="h-full w-full max-w-6xl">
+                <AccessControl>{children}</AccessControl>
+              </div>
+            </main>
+          </ScrollArea>
+        </div>
+        <div
+          className={`relative hidden w-full flex-col transition-all duration-300 ease-in-out xl:flex ${
+            isInboxCollapsed ? 'max-w-16' : 'max-w-64'
+          }`}
+        >
+          <Inbox isCollapsed={isInboxCollapsed} toggleSidebar={toggleInbox} />
+        </div>
       </div>
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <Header />
-        <ScrollArea className="flex-1">
-          <main className="mx-4 flex h-full w-full items-center justify-center py-4 2xl:mx-auto">
-            <div className="h-full w-full max-w-6xl">
-              <AccessControl>{children}</AccessControl>
-            </div>
-          </main>
-        </ScrollArea>
-      </div>
-      <div
-        className={`relative hidden w-full flex-col transition-all duration-300 ease-in-out xl:flex ${
-          isInboxCollapsed ? "max-w-16" : "max-w-64"
-        }`}
-      >
-        <Inbox isCollapsed={isInboxCollapsed} toggleSidebar={toggleInbox} />
-      </div>
-    </div>
+    </TooltipProvider>
   );
 }
