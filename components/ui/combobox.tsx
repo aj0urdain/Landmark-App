c378df47fb71e3ee67ea8528298eceef48a57c74
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-10-29 22:16:45 +1100

    chore: update UI components and VS Code configuration

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import * as React from 'react';
+import { Check, ChevronsUpDown } from 'lucide-react';
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 import {
   Command,
   CommandEmpty,
@@ -12,18 +12,16 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import Image from "next/image";
+} from '@/components/ui/command';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import Image from 'next/image';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 
 export interface ComboboxOption {
   value: string;
-  label: string;
+  label: string | React.ReactNode;
   imageUrl?: string;
+  disabled?: boolean;
 }
 
 interface ComboboxProps {
@@ -32,6 +30,7 @@ interface ComboboxProps {
   onSelect: (value: string) => void;
   selectedValue?: string;
   className?: string;
+  avatar?: boolean;
 }
 
 export function Combobox({
@@ -40,6 +39,7 @@ export function Combobox({
   onSelect,
   selectedValue,
   className,
+  avatar = false,
 }: ComboboxProps) {
   const [open, setOpen] = React.useState(false);
 
@@ -50,17 +50,32 @@ export function Combobox({
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className={cn("w-full justify-between", className)}
+          className={cn('w-full justify-between', className)}
         >
-          {selectedValue
-            ? options.find((option) => option.value === selectedValue)?.label
-            : placeholder}
+          <div className="flex items-center gap-2">
+            <Avatar className="border-muted border flex items-center w-5 h-5">
+              <AvatarImage
+                src={
+                  options.find((option) => option.value === selectedValue)?.imageUrl ?? ''
+                }
+                alt={
+                  options.find((option) => option.value === selectedValue)
+                    ?.label as string
+                }
+                className="h-full w-full object-cover"
+              />
+            </Avatar>
+            {selectedValue
+              ? options.find((option) => option.value === selectedValue)?.label
+              : placeholder}
+          </div>
+
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent
         className="w-full p-0"
-        style={{ width: "var(--radix-popover-trigger-width)" }}
+        style={{ width: 'var(--radix-popover-trigger-width)' }}
       >
         <Command>
           <CommandInput placeholder="Search..." />
@@ -72,30 +87,39 @@ export function Combobox({
                   key={option.value}
                   value={option.value}
                   onSelect={(currentValue) => {
-                    onSelect(
-                      currentValue === selectedValue ? "" : currentValue,
-                    );
-                    setOpen(false);
+                    if (!option.disabled) {
+                      onSelect(currentValue === selectedValue ? '' : currentValue);
+                      setOpen(false);
+                    }
                   }}
+                  disabled={option.disabled}
+                  className={cn(option.disabled && 'cursor-not-allowed opacity-50')}
                 >
-                  <div className="flex items-center">
-                    {option.imageUrl && (
-                      <Image
-                        src={option.imageUrl}
-                        alt={option.label}
-                        width={100}
-                        height={100}
-                        className="mr-2 h-5 w-5 object-contain"
-                      />
-                    )}
+                  <div className="flex items-center gap-2">
+                    {option.imageUrl &&
+                      (avatar ? (
+                        <Avatar className="border-muted border flex items-center w-5 h-5">
+                          <AvatarImage
+                            src={option.imageUrl}
+                            alt={option.label as string}
+                            className="h-full w-full object-cover"
+                          />
+                        </Avatar>
+                      ) : (
+                        <Image
+                          src={option.imageUrl}
+                          alt={option.label as string}
+                          width={100}
+                          height={100}
+                          className="mr-2 h-5 w-5 object-contain"
+                        />
+                      ))}
                     <span>{option.label}</span>
                   </div>
                   <Check
                     className={cn(
-                      "ml-auto h-4 w-4",
-                      selectedValue === option.value
-                        ? "opacity-100"
-                        : "opacity-0",
+                      'ml-auto h-4 w-4',
+                      selectedValue === option.value ? 'opacity-100' : 'opacity-0',
                     )}
                   />
                 </CommandItem>
