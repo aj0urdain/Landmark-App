2cc6ae4c17018dd0f32a779ff5e425940ba7d3ad
Aaron J. Girton <agirton@intradark.com>
2024-10-31 19:02:56 +1100

    chore: remove deprecated files and update announcements page

--- app/(main)/news/announcements/page.tsx
+++ app/(main)/news/announcements/page.tsx
@@ -1,7 +1,120 @@
+'use client';
+
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { createArticle } from '@/utils/use-cases/articles/createArticle';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useToast } from '@/hooks/use-toast';
+import { createBrowserClient } from '@/utils/supabase/client';
+import { Card } from '@/components/ui/card';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const AnnouncementsNewsPage = () => {
-  return <div>AnnouncementsNewsPage</div>;
+  const router = useRouter();
+  const { toast } = useToast();
+  const supabase = createBrowserClient();
+
+  const createAnnouncementMutation = useMutation({
+    mutationFn: () => createArticle('announcement'),
+    onSuccess: (newArticleId: number) => {
+      router.push(`/news/announcements/${newArticleId.toString()}`);
+    },
+    onError: (error) => {
+      console.error('Error creating announcement:', error);
+      toast({
+        title: 'Error creating announcement!',
+        description: 'Please try again later',
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const getAnnouncements = async () => {
+    const { data, error } = await supabase
+      .from('articles')
+      .select('*')
+      .eq('article_type', 'announcement');
+
+    return data;
+  };
+
+  const {
+    data: announcements,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['announcements'],
+    queryFn: getAnnouncements,
+  });
+
+  return (
+    <div className="">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Announcements</h1>
+        <Button
+          onClick={() => {
+            createAnnouncementMutation.mutate();
+          }}
+          disabled={createAnnouncementMutation.isPending}
+        >
+          <Plus className="w-4 h-4 mr-2" />
+          {createAnnouncementMutation.isPending
+            ? 'Creating Announcement...'
+            : 'Create Announcement'}
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {announcements &&
+          announcements.length > 0 &&
+          announcements.map((announcement) => (
+            <Link
+              href={`/news/announcements/${announcement.id.toString()}`}
+              key={announcement.id}
+            >
+              <Card className="w-full relative min-h-80 flex flex-col items-start justify-end p-6 gap-2">
+                <div className="">
+                  <Image
+                    src={announcement.cover_image ?? ''}
+                    alt={announcement.title ?? ''}
+                    width={1000}
+                    height={1000}
+                    className=" absolute top-0 left-0 w-full h-full object-cover object-center rounded-xl"
+                  />
+                  <div className="absolute inset-0 bg-gradient-to-b from-background/50 via-background/75 to-background rounded-xl" />
+                </div>
+
+                <h1 className="text-2xl font-black z-10 line-clamp-2">
+                  {announcement.title}
+                </h1>
+
+                <p className="line-clamp-1 font-medium z-10 text-muted-foreground">
+                  {announcement.description}
+                </p>
+              </Card>
+            </Link>
+          ))}
+      </div>
+
+      <div className="mt-6">
+        <h1 className="text-xl font-bold">More Announcements</h1>
+        <div className="grid grid-cols-1 gap-4 mt-6">
+          {/* Dummy data for list of additional articles */}
+          {new Array(10).fill(0).map((_, index) => (
+            <Card key={index} className="w-full min-h-24 rounded-lg p-6">
+              <h1 className="text-lg font-bold">Article title {index + 1}</h1>
+              <p className="text-sm text-muted-foreground">
+                Article description {index + 1}
+              </p>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AnnouncementsNewsPage;
