4c0f0c1f7a26cf604e5fa9d4f1a206206307aaab
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-11-05 21:44:37 +1100

    feat(auth): implement email/password authentication alternative

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,60 +1,226 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import { signInWithPasswordAction } from "@/app/actions";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { ReloadIcon } from "@radix-ui/react-icons";
-import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState, useEffect, useRef } from 'react';
+import { signInAction, verifyOtpAction } from '@/app/actions';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useRouter } from 'next/navigation';
 import {
-  FormMessage,
-  Message,
-} from "@/components/atoms/FormMessage/FormMessage";
-import { createBrowserClient } from "@/utils/supabase/client";
-import { Dot } from "@/components/atoms/Dot/Dot";
-import { LogoWordmark } from "@/components/atoms/LogoWordmark/LogoWordmark";
-import { Separator } from "@/components/ui/separator";
-import { Badge } from "@/components/ui/badge";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
+import { createBrowserClient } from '@/utils/supabase/client';
+import { FormMessage, Message } from '@/components/atoms/FormMessage/FormMessage';
+import Image from 'next/image';
+import { Card } from '@/components/ui/card';
+import { LogoWordmark } from '@/components/atoms/LogoWordmark/LogoWordmark';
+import { Separator } from '@/components/ui/separator';
+import { Dot } from '@/components/atoms/Dot/Dot';
 
-type ActionResult = { error: string } | { success: string };
+type ActionResult =
+  | { error: string; attemptsLeft?: number; cooldownMinutes?: number }
+  | { success: string };
 
-export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState<Message>({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLoginSuccessful, setIsLoginSuccessful] = useState(false);
-  const [allowedDomains, setAllowedDomains] = useState<string[]>([]);
-  const [showPassword, setShowPassword] = useState(false);
+const SignInForm = ({
+  allowedDomains,
+  isOtpSent,
+  onSubmit,
+  attemptsLeft,
+  isLoading,
+  otpError,
+  onReset,
+  email,
+  isLoginSuccessful,
+}: {
+  allowedDomains: string[];
+  isOtpSent: boolean;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>, otp?: string) => Promise<void>;
+  attemptsLeft: number | null;
+  isLoading: boolean;
+  otpError: boolean;
+  onReset: () => void;
+  email: string;
+  isLoginSuccessful: boolean;
+}) => {
+  const [otpValue, setOtpValue] = useState('');
+  const [currentEmail, setCurrentEmail] = useState(email);
+  const formRef = useRef<HTMLFormElement>(null);
+  const isFirstAttemptRef = useRef(true);
 
-  const router = useRouter();
+  useEffect(() => {
+    if (
+      isOtpSent &&
+      otpValue.length === 6 &&
+      isFirstAttemptRef.current &&
+      formRef.current
+    ) {
+      formRef.current.requestSubmit();
+      isFirstAttemptRef.current = false;
+    }
+  }, [otpValue, isOtpSent]);
+
+  useEffect(() => {
+    if (otpError) {
+      setOtpValue('');
+    }
+  }, [otpError]);
 
   const isAllowedDomain = (email: string) => {
-    const domain = email.split("@")[1];
+    const domain = email.split('@')[1];
     return domain && allowedDomains.includes(domain);
   };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  return (
+    <form
+      ref={formRef}
+      className="flex min-w-64 flex-1 flex-col gap-8"
+      onSubmit={(e) => onSubmit(e, otpValue)}
+    >
+      <h1 className="text-2xl font-medium">Sign in</h1>
+      <div className="flex w-full flex-col items-start gap-2">
+        <div className="w-full">
+          <Label htmlFor="email">Email</Label>
+
+          <Input
+            name="email"
+            placeholder="you@example.com"
+            required
+            className="w-full"
+            disabled={isOtpSent || isLoading}
+            defaultValue={email}
+            onChange={(e) => setCurrentEmail(e.target.value)}
+          />
+
+          <input type="hidden" name="email" value={currentEmail} />
+
+          {isOtpSent && (
+            <Button
+              variant="link"
+              size="icon"
+              className="-mt-2 ml-2 text-xs text-muted-foreground underline"
+              onClick={onReset}
+            >
+              Not you?
+            </Button>
+          )}
+        </div>
+
+        <div className="w-full">
+          {isOtpSent && (
+            <>
+              <Label htmlFor="token">Sign In Code</Label>
+              <InputOTP
+                maxLength={6}
+                value={otpValue}
+                onChange={(value) => setOtpValue(value)}
+                name="token"
+                disabled={attemptsLeft === 0}
+                className="w-full"
+              >
+                <InputOTPGroup className="w-full">
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <InputOTPSlot
+                      key={index}
+                      index={index}
+                      className={`${
+                        otpError && otpValue.length === 0 && 'border-destructive'
+                      } w-full`}
+                    />
+                  ))}
+                </InputOTPGroup>
+              </InputOTP>
+            </>
+          )}
+        </div>
+
+        <div className="w-full">
+          {isLoading || isLoginSuccessful ? (
+            <Button disabled className="w-full">
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+              {isLoginSuccessful ? 'Logging in...' : 'Please wait'}
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className={`w-full ${
+                isOtpSent && (otpValue.length < 6 || attemptsLeft === 0)
+                  ? 'cursor-not-allowed bg-muted text-muted-foreground'
+                  : ''
+              }`}
+              disabled={
+                (isOtpSent && (otpValue.length < 6 || attemptsLeft === 0)) ||
+                (!isOtpSent && !isAllowedDomain(currentEmail)) ||
+                (!isOtpSent && attemptsLeft === 0)
+              }
+            >
+              {isOtpSent ? 'Verify Code' : 'Send Code'}
+            </Button>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default function SignIn() {
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [message, setMessage] = useState<Message>({});
+  const [attemptsLeft, setAttemptsLeft] = useState<number | null>(null);
+  const [cooldownMinutes, setCooldownMinutes] = useState(5);
+  const [isLoading, setIsLoading] = useState(false);
+  const [otpError, setOtpError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [allowedDomains, setAllowedDomains] = useState<string[]>([]);
+  const [isLoginSuccessful, setIsLoginSuccessful] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, otp?: string) => {
     event.preventDefault();
     setIsLoading(true);
+    setOtpError(false);
     const formData = new FormData(event.currentTarget);
+    if (otp) formData.set('token', otp);
+    if (!isOtpSent) {
+      setEmail(formData.get('email') as string);
+    }
     try {
-      const result = (await signInWithPasswordAction(formData)) as ActionResult;
+      const result = (await (isOtpSent
+        ? verifyOtpAction(formData)
+        : signInAction(formData))) as ActionResult;
 
-      if ("error" in result) {
+      if ('error' in result) {
         setMessage({ error: result.error });
-      } else if ("success" in result) {
+        if (result.attemptsLeft !== undefined) {
+          setAttemptsLeft(result.attemptsLeft);
+        }
+        if (result.cooldownMinutes !== undefined) {
+          setCooldownMinutes(result.cooldownMinutes);
+        }
+        if (isOtpSent) {
+          setOtpError(true);
+        }
+      } else if ('success' in result) {
         setMessage({ success: result.success });
-        setIsLoginSuccessful(true);
-        router.push("/");
+        if (isOtpSent) {
+          setIsLoginSuccessful(true);
+          // Delay redirect to allow UI to update
+
+          router.push('/');
+        } else {
+          setIsOtpSent(true);
+          setAttemptsLeft(null); // Reset attempts left when OTP is sent
+        }
       }
     } catch (error) {
-      console.error("Error during submission:", error);
-      setMessage({ error: "An unexpected error occurred" });
+      console.error('Error during submission:', error);
+      setMessage({ error: 'An unexpected error occurred' });
     } finally {
       if (!isLoginSuccessful) {
         setIsLoading(false);
@@ -62,21 +228,34 @@ export default function SignIn() {
     }
   };
 
+  const isDisabled = isOtpSent && attemptsLeft === 0;
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  const handleReset = () => {
+    setIsOtpSent(false);
+    setMessage({});
+    setAttemptsLeft(null);
+    setOtpError(false);
+  };
+
   useEffect(() => {
     const fetchAllowedDomains = async () => {
       const supabase = createBrowserClient();
       const { data, error } = await supabase
-        .from("app_config")
-        .select("value")
-        .eq("key", "ALLOWED_DOMAINS")
+        .from('app_config')
+        .select('value')
+        .eq('key', 'ALLOWED_DOMAINS')
         .single();
 
       if (data && !error) {
         const allowedDomainsArray = data.value;
-
+        console.log('Allowed domains:', allowedDomainsArray);
         setAllowedDomains(allowedDomainsArray as string[]);
       } else {
-        console.error("Error fetching allowed domains:", error);
+        console.error('Error fetching allowed domains:', error);
       }
     };
 
@@ -84,119 +263,66 @@ export default function SignIn() {
   }, []);
 
   return (
-    <div className="flex h-screen w-full">
-      <div className="relative hidden w-1/2 bg-muted lg:block">
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="relative flex h-full w-1/2">
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-gradient-to-t from-slate-800 to-transparent" />
         <Image
           src="/images/burgess-rawson-login-bg.jpg"
-          alt="Burgess Rawson Login Background"
+          alt="logo"
           width={2048}
           height={1365}
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="h-full w-full object-cover opacity-40 grayscale"
         />
-        <div className="w-full">
-          <div className="absolute inset-0 flex w-full flex-col items-center justify-center">
-            <div className="w-full max-w-xl">
-              <LogoWordmark />
-            </div>
-
-            <Separator className="my-8 w-full max-w-lg bg-foreground" />
-
-            <div className="flex items-center justify-center gap-8">
-              <div className="flex items-center gap-2">
-                <Dot size="small" className="bg-white" />
-                <Dot size="large" className="bg-white" />
-                <div className="font-lexia text-4xl font-extrabold uppercase tracking-wider text-white">
-                  Landmark
-                </div>
-                <Dot size="large" className="bg-white" />
-                <Dot size="small" className="bg-white" />
-              </div>
-              <div>
-                <Badge
-                  className="animate-pulse bg-blue-500 px-4 text-sm uppercase text-white"
-                  variant="outline"
-                >
-                  Beta
-                </Badge>
-              </div>
-            </div>
+        <div className="absolute left-0 top-0 z-20 flex h-full w-full flex-col items-center justify-center gap-8">
+          <LogoWordmark className="h-auto w-2/3" />
+          <Separator orientation="vertical" className="h-0.5 w-1/2 bg-white" />
+          <div className="flex items-center justify-center gap-6">
+            <Dot size="small" />
+            <Dot size="large" />
+            <p className="font-lexia text-4xl font-bold uppercase tracking-widest">
+              Landmark
+            </p>
+            <Dot size="large" />
+            <Dot size="small" />
           </div>
         </div>
       </div>
-      <div className="flex w-full items-center justify-center lg:w-1/2">
-        <div className="mx-auto w-full max-w-[380px] px-4">
-          <div className="grid gap-10">
-            <div className="grid gap-2">
-              <h1 className="text-3xl font-bold">Login</h1>
-              <p className="text-balance text-sm text-muted-foreground">
-                Enter your email below to log in to Landmark.
+      <div className="flex h-full w-1/2 items-center justify-center">
+        <Card className="max-h-96 w-full max-w-sm p-6">
+          <div className="flex w-full flex-col gap-2">
+            <SignInForm
+              isOtpSent={isOtpSent}
+              onSubmit={handleSubmit}
+              attemptsLeft={attemptsLeft}
+              isLoading={isLoading}
+              otpError={otpError}
+              onReset={handleReset}
+              email={email}
+              allowedDomains={allowedDomains}
+              isLoginSuccessful={isLoginSuccessful}
+            />
+            <FormMessage message={message} />
+            {isOtpSent && attemptsLeft !== null && attemptsLeft > 0 && (
+              <p className="text-xs text-warning dark:text-warning-foreground">
+                Attempts left: {attemptsLeft}
               </p>
-            </div>
-            <form onSubmit={handleSubmit} className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="you@example.com"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="#"
-                    className="ml-auto inline-block text-sm text-muted-foreground underline"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Forgot your password?
-                  </Link>
+            )}
+            <Dialog open={isDisabled}>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Too Many Attempts</DialogTitle>
+                  <DialogDescription>
+                    You have made too many false attempts. You can try again in{' '}
+                    {cooldownMinutes} minutes.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="mt-6 flex justify-center">
+                  <Button onClick={handleRefresh}>I understand</Button>
                 </div>
-                <div className="flex gap-2">
-                  <Input
-                    id="password"
-                    name="password"
-                    type={showPassword ? "text" : "password"}
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <Button
-                    type="button"
-                    variant="outline"
-                    size="icon"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {!showPassword ? (
-                      <EyeOffIcon className="h-4 w-4" />
-                    ) : (
-                      <EyeIcon className="h-4 w-4" />
-                    )}
-                  </Button>
-                </div>
-              </div>
-              {isLoading || isLoginSuccessful ? (
-                <Button disabled className="w-full">
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                  {isLoginSuccessful ? "Logging in..." : "Please wait"}
-                </Button>
-              ) : (
-                <Button
-                  type="submit"
-                  className="w-full"
-                  disabled={!email || !password || !isAllowedDomain(email)}
-                >
-                  Login
-                </Button>
-              )}
-              <FormMessage message={message} />
-            </form>
+              </DialogContent>
+            </Dialog>
           </div>
-        </div>
+        </Card>
       </div>
     </div>
   );
