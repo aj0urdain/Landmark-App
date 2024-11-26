3d813c97d6dcee189103d3d8485cf59d44bd18a7
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-11-26 22:53:33 +1100

    feat(portfolio): enhance document generator page

--- app/(main)/sandbox/document-generator/portfolio-page/page.tsx
+++ app/(main)/sandbox/document-generator/portfolio-page/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import PortfolioPageContent from '@/components/templates/PortfolioPageContent/PortfolioPageContent';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
 const PortfolioPageWrapper = () => {
-  return <PortfolioPageContent />;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  return <PortfolioPageContent searchParams={searchParams} router={router} />;
 };
 
 const PortfolioPage = () => {
